219ed4bb3352243210a272fea7bb154427f08be4
ulilnerd <72185621+ulilnerd@users.noreply.github.com>
2020-10-05 23:55:35 -0700

    Feature: can now play music

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
+const ytdl = require('ytdl-core');
 
 const client = new Discord.Client();
 const prefix = "!";
+const queue = new Map();
 
 client.on("message", function(message) { 
     if (message.author.bot) return;   
@@ -11,6 +13,8 @@ client.on("message", function(message) {
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
+    const serverQueue = queue.get(message.guild.id);
+
 
     if (command === "ping") {
         const timeTaken = Date.now() - message.createdTimestamp;
@@ -20,12 +24,113 @@ client.on("message", function(message) {
         const numArgs = args.map(x => parseFloat(x));
         const sum = numArgs.reduce((counter, x) => counter += x);
         message.reply(`The sum of all the arguments you provided is ${sum}!`);     
-    }             
+    } 
+    else if (command === "play") {
+        execute(message, serverQueue);
+        return;
+    }
+    else if (command === "skip") {
+        skip(message, serverQueue);
+        return;
+    }   
+    else if (command === "stop") {
+        stop(message, serverQueue);
+        return;
+    }
+    else {
+        message.reply("Sorry I'm not sure what you're trying to say ¯\_(ツ)_/¯ ")
+    }         
 });    
+
+async function execute(message, serverQueue) {
+    const args = message.content.split(" ");
+  
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+      return message.channel.send(
+        "You need to be in a voice channel to play music!"
+      );
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+      return message.channel.send(
+        "I need the permissions to join and speak in your voice channel!"
+      );
+    }
+  
+
+const songInfo = await ytdl.getInfo(args[1]);
+const song = {
+ title: songInfo.title,
+ url: songInfo.video_url,
+};
+
+if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+
+    queue.set(message.guild.id, queueContruct);
+
+    queueContruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+} else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`${song.title} has been added to the queue!`);
+  }
+}
+
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  if (!serverQueue)
+    return message.channel.send("There is no song that I could skip!");
+  serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`Started playing: **${song.title}**`);
+}
+
 
 client.login(config.BOT_TOKEN);
 console.log("SpongeBot is now online!")
-
-
-                                  
-
